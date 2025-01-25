4f554556ec5733d0d3a169f1e271821a50bd81aa
yamushka <joanjode@student.arteveldehs.be>
2025-01-25 16:09:04 +0100

    added $ to variables

--- static/js/galleries.js
+++ static/js/galleries.js
@@ -56,26 +56,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // lightbox gallery 
 document.addEventListener("DOMContentLoaded", () => {
-  const images = document.querySelectorAll(".gallery img");
-  const lightbox = document.getElementById("lightbox");
-  const lightboxImg = document.querySelector(".lightbox__image");
-  const closeButton = document.querySelector(".lightbox__close");
+  const $images = document.querySelectorAll(".gallery img");
+  const $lightbox = document.getElementById("lightbox");
+  const $lightboxImg = document.querySelector(".lightbox__image");
+  const $closeButton = document.querySelector(".lightbox__close");
 
-  images.forEach((img) => {
+  $images.forEach((img) => {
     img.addEventListener("click", () => {
-      lightbox.style.display = "flex";
-      lightboxImg.src = img.src;
-      lightboxImg.alt = img.alt;
+      $lightbox.style.display = "flex";
+      $lightboxImg.src = img.src;
+      $lightboxImg.alt = img.alt;
     });
   });
 
-  closeButton.addEventListener("click", () => {
-    lightbox.style.display = "none";
+  $closeButton.addEventListener("click", () => {
+    $lightbox.style.display = "none";
   });
 
-  lightbox.addEventListener("click", (e) => {
-    if (e.target === lightbox && e.target !== lightboxImg) {
-      lightbox.style.display = "none";
+  $lightbox.addEventListener("click", (e) => {
+    if (e.target === $lightbox && e.target !== $lightboxImg) {
+      $lightbox.style.display = "none";
     }
   });
 });
